eee7c0d863aeb577412cf6abc31d0550572eae5d
Aurella Meidista Septiasa <aurellinda703@gmail.com>
2026-05-03 11:04:24 +0700

    tambah

---
+++ views/tambah.php
@@ -0,0 +1,19 @@
+<?php
+include_once "../contr/menucontr.php";
+$contr = new menucontr();
+if(isset($_POST[simpan])) {
+    $contr->model->tambah($_POST['minuman'], $_POST['harga']);
+    header("location:../index.php");
+}
+?>
+
+<h2>Tambah Mahasiswa</h2>
+<form method="POST">
+    Minuman:
+    <input type="text" name="minuman">
+    <br><br>
+    Harga:
+    <input type="text" name="harga">
+    <br><br>
+    <button type="submit" name="simpan">Simpan</button>
+</form>
